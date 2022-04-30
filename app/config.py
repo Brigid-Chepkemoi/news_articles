c2b26cdd94aa21d39eb8e3038648f05f2a7c0001
brie <briememo95@gmail.com>
2022-04-30 13:46:29 +0300

    add changes

---
+++ app/config.py
@@ -0,0 +1,12 @@
+from distutils.debug import DEBUG
+
+
+class Config:
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/{}?apiKey={}'
+
+class ProdConfig(Config):
+    pass
+
+class DevConfig(Config):
+
+    DEBUG = True
