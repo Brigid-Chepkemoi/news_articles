1dcf0b57ba762010bbe2126b6a694cfecd0bc7f3
brie <briememo95@gmail.com>
2022-05-02 21:02:33 +0300

    add formatting

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ from distutils.debug import DEBUG
 
 
 class Config:
-    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?/{}?&language=en&from=2022-04-16&apiKey={}'
+    # NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?/{}?&language=en&from=2022-04-16&apiKey={}'
+    NEWS_API_BASE_URL= 'https://newsapi.org/v2/top-headlines/sources?category={}&language=en&from=2022-04-16&apiKey={}'
 
 class ProdConfig(Config):
     pass
--- app/request.py
+++ app/request.py
@@ -11,25 +11,26 @@ base_url = app.config['NEWS_API_BASE_URL']
 def get_news(category):
     get_news_url = base_url.format(category,NEWS_API_KEY)
 
-    with urllib.request.urlopen(get_news_url) as url:
-        get_news_data =url.read()
-        get_news_response = json.loads(get_news_data)
+    #with urllib.request.urlopen(get_news_url).read()  as url:
+        # get_news_data =url.read()
+    get_news_data=urllib.request.urlopen(get_news_url).read()
+    get_news_response = json.loads(get_news_data)
 
-        if get_news_response['sources']:
-            news_results_list= get_news_response['sources']
-            news_results = process_results(news_results_list)
+    if get_news_response['sources']:
+        news_results_list= get_news_response['sources']
+        #news_results = process_results(news_results_list)
     
-    return news_results
+    return news_results_list;
 
-def process_results(news_list):
-    news_results = []
-    for news_item in news_list:
-        id = news_item.get('id')
-        name = news_item.get('name')
-        description = news_item.get('description')
-        url = news_item.get('url')
-        category = news_item.get('category')
-        if url:
-            news_object = News(id,name,description,url,category)
-            news_results.append(news_object)  
-    return news_results
+# def process_results(news_list):
+#     news_results = []
+#     for news_item in news_list:
+#         id = news_item.get('id')
+#         name = news_item.get('name')
+#         description = news_item.get('description')
+#         url = news_item.get('url')
+#         category = news_item.get('category')
+#         if url:
+#             news_object = News(id,name,description,url,category)
+#             news_results.append(news_object)  
+#     return news_results
